72c2262134a1807a3d8b81dd694160423a49144c
david.zoroastro.evangelista@gmail.com <EduardA14122012>
2020-05-20 19:49:14 -0300

    Organizando Imports

--- src/main/java/br/com/aisdigital/prova/client/MovieClient.java
+++ src/main/java/br/com/aisdigital/prova/client/MovieClient.java
@@ -4,10 +4,6 @@ import br.com.aisdigital.prova.dto.MovieDTO;
 import br.com.aisdigital.prova.dto.ResponseList;
 import feign.Param;
 import feign.RequestLine;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.cloud.openfeign.FeignClient;
 
 import java.util.Optional;
